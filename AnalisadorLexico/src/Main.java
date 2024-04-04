38652ed57f5b3cde06c1fb58ddb851fa1a9b4900
Victor Dantas <99653338+victormcdantas02@users.noreply.github.com>
2024-04-04 14:03:01 -0300

    Main.java

--- AnalisadorLexico/src/Main.java
+++ AnalisadorLexico/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         AnalisadorLexico analisador = new AnalisadorLexico();
         String codigoFonte = "int main() { println(\"Olá, mundo!\"); return 0; }";
+        codigoFonte = "int main() { int numero1 = 123; int numero2 = 456; int resultado = numero1 + numero2; return resultado; } float calcularMedia(float nota1, float nota2) { float media = (nota1 + nota2) / 2.0; if (media >= 6.0) { println(\"Aprovado!\"); } else { println(\"Reprovado.\"); } return media; } void saudacao() { char mensagem[] = \"Olá, mundo!\"; int numero = 42; println(mensagem); println(\"O número é: \" + numero); } int dobrar(int x) { return x * 2; } void imprimirValores(int valores[]) { for (int i = 0; i < valores.length; i++) { println(\"Valor[\" + i + \"] = \" + valores[i]); } }\n";
         List<Token> tokens = analisador.analisar(codigoFonte);
 
         for (Token token : tokens) {
